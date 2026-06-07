413a80e893181cd1173e915e776fdd27d592b586
Aditya Mishra <adityamishra3337@gmail.com>
2026-06-07 12:44:25 +0530

    Update createpost.jsx

--- src/pages/createpost.jsx
+++ src/pages/createpost.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
+import { useAuth } from "../context/AuthContext"
 
 const CreatePost = () => {
     const [text, setText] = useState("")
     const [image, setImage] = useState(null)
     const navigate = useNavigate()
+    const { user } = useAuth()
+
+    useEffect(() => {
+        if(!localStorage.getItem('token')) {
+            navigate('/login')
+        }
+    }, [])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
